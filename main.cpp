667c8db1f7b6064f2792fc9b1784a769c94ba1dd
unknown <Olivier@Quantum.(none)>
2011-11-14 10:10:38 +0100

    suppression du prototype special pour Windows: inutile et non reconnu par ce dernier

--- main.cpp
+++ main.cpp
@@ -3,13 +3,7 @@
 
 using namespace std;
 
-#ifdef WIN32
-//windows need their WinMain :-/
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd){
-    StoreHinstance(hInstance);
-#else
 int main(int argc, char* argv[] ){
-#endif
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		cerr << "Erreur à l'initialisation de la SDL : " << SDL_GetError() << endl;;
 		exit(EXIT_FAILURE);
@@ -23,4 +17,6 @@ int main(int argc, char* argv[] ){
 		cerr << e.what() << endl;
 		exit(EXIT_FAILURE);
 	}
+
+	return EXIT_SUCCESS;
 }
