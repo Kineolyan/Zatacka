4d76e586e8841ccd5f2d15a2c70057e4b9712a20
Olivier <kineolyan@hotmail.fr>
2011-11-19 21:14:04 +0100

    Creation d'une classe pour gérer l'écran de jeu, l'affichage des scores

--- jeu/jeu.h
+++ jeu/jeu.h
@@ -9,48 +9,29 @@
 #define JEU_H_
 
 #include <SDL/SDL.h>
-#include <map>
-#include <vector>
+
+#include "itemEcran.h"
 #include "texte.h"
-#include "option.h"
-#include "../exceptions/exception.h"
+#include "couleurs.h"
+#include "zatacka.h"
+#include "../serpent/serpent.h"
 
-/**
- * Instance de jeu gerant les différents écrans, les serpents, les options
- */
-class Jeu {
+class Jeu: public ItemEcran {
 private:
-	enum NomEcran {
-		ACCUEIL, MENU_PRINCIPAL, MENU_OPTIONS, JEU
-	};
-
-public:
-	enum Couleur {
-		BLANC=0, JAUNE=1, BLEU=2, ROUGE=3, VERT=4, VIOLET=5, ORANGE=6,
-		NOIR=7
-	};
-
-private:
-
-	/**
-	 * Largeur de la fenetre de jeu
-	 */
-	int m_largeur;
-
-	/**
-	 * Hauteur de la fenetre
-	 */
-	int m_hauteur;
-
 	/**
 	 * Largeur du panneau des scores
 	 */
 	int m_largeurScores;
 
 	/**
-	 * Ecran principal
+	 * Position des scores à l'écran
 	 */
-	SDL_Surface* m_ecran;
+	SDL_Rect m_positionScores;
+
+	/**
+	 * Ecran principal du jeu
+	 */
+	Zatacka& m_ecranPrincipal;
 
 	/**
 	 * Ecran de jeu contenant les serpents
@@ -65,37 +46,7 @@ private:
 	/**
 	 * Motifs de base pour le trace des serpents
 	 */
-	std::vector<SDL_Surface*> m_points;
-
-	/**
-	 * Ecran à afficher
-	 */
-	NomEcran m_ecranAAfficher;
-
-	/**
-	 * Police calligraphiee pour l'accueil et les scores
-	 */
-	TTF_Font* m_policeCalligraphiee;
-
-	/**
-	 * Police basique pour les textes de menu
-	 */
-	TTF_Font* m_policeBasique;
-
-	/**
-	 * Tableau des couleurs rgb à utiliser
-	 */
-	std::vector<SDL_Color*> m_couleurs;
-
-	/**
-	 * Options pour activer ou pas un joueur
-	 */
-	std::vector<Option*> m_optionJoueurs;
-
-	/**
-	 * Options pour activer les différentes règles de jeu
-	 */
-	std::vector<Option*> m_options;
+	SDL_Surface* m_points[6];
 
 	/**
 	 * Tableau des textes affichant les scores sur l'écran
@@ -104,81 +55,28 @@ private:
 	TexteSDL m_scores[6];
 
 	/**
-	 * Instancie les polices en chargeant le fichier et définit la taille
+	 * Tableau des serpents des joueurs
 	 */
-	void chargerPolices();
+	Serpent* m_joueurs;
 
 	/**
-	 * Crée la liste des couleurs, en donnant les codes RGB
+	 * Donne à chaque score une couleur, une police et une position
 	 */
-	void initialiserCouleurs();
+	void initialiserPoints();
 
 	/**
 	 * Donne à chaque score une couleur, une police et une position
 	 */
 	void initialiserScores();
 
-	/**
-	 * Créer les options du menu principal et leur assigne une position à
-	 * l'écran
-	 */
-	void creerMenuPrincipal();
-
-	/**
-	 * Crée les options du menu d'options et leur assigne une position.
-	 */
-	void creerMenuOptions();
-
-	/**
-	 * Crée l'écran de jeu avec avec la zone de jeu et le panneau des scores.
-	 * Les motifs utilises pour tracer les serpents sont egalement instancies.
-	 *
-	 * @throw InstanceManquante
-	 */
-	void creerJeu();
-
-	/**
-	 * Affiche l'écran d'accueil
-	 */
-	void afficherEcranPrincipal();
-
-	/**
-	 * Affiche le menu principal
-	 */
-	void afficherMenuPrincipal();
-
-	/**
-	 * Affiche le menu des options
-	 */
-	void afficherMenuOptions();
-
-	/**
-	 * Affiche l'écran de jeu
-	 */
-	void afficherJeu();
-
-	/**
-	 * Afficher l'écran demandé
-	 * @param ecran: nom de l'écran à afficher
-	 */
-	void afficher(NomEcran ecran);
-
-	/**
-	 * Efface l'écran
-	 */
-	void effacer();
-
 public:
-	/**
-	 * Constructeur
-	 * @param largeur: largeur de la fenetre
-	 * @param hauteur: hauteur de la fenetre
-	 */
-	Jeu(int largeur, int hauteur);
-	/**
-	 * Destructeur
-	 */
+	Jeu(Zatacka& ecranPrincipal, int largeurScore=100);
 	~Jeu();
+
+	virtual void afficher(SDL_Surface* ecran);
+	void afficherJeu(SDL_Surface* ecran);
+	void afficherScores(SDL_Surface* ecran);
+	void demarrerManche();
 
 	/**
 	 * Dessine un point d'une couleur donnée sur l'écran de jeu.
