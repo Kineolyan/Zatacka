1664d31358c93b39c6cea5d671fb8e49be316437
Olivier <kineolyan@hotmail.fr>
2011-11-19 21:11:44 +0100

    Separation des couleurs dans un fichier facile à inclure

---
+++ jeu/couleurs.h
@@ -0,0 +1,17 @@
+/*
+ * couleurs.h
+ *
+ *  Created on: 19 nov. 2011
+ *      Author: oliv
+ */
+
+#ifndef COULEURS_H_
+#define COULEURS_H_
+
+
+enum Couleur {
+	BLANC=6, JAUNE=1, BLEU=5, ROUGE=0, VERT=3, VIOLET=4, ORANGE=2,
+	NOIR=7
+};
+
+#endif /* COULEURS_H_ */
