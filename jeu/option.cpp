0f6d43eb2ff8d3ac8ac41e25e9c142b6370d69a8
Olivier <kineolyan@hotmail.fr>
2011-11-21 09:22:16 +0100

    Adaptation de Option à la class Ecran

--- jeu/option.cpp
+++ jeu/option.cpp
@@ -4,18 +4,23 @@ using namespace std;
 
 Option::Option(string texte, string optionActive, string optionInactive,
 		TTF_Font* police, SDL_Color* couleur):
-		ItemEcran(),
+		Ecran(0, 0),
 		m_texte(texte, police, couleur),
 		m_optionActive(optionActive, police, couleur),
 		m_optionInactive(optionInactive, police, couleur),
-		m_blocEffaceur(NULL),
 		m_active(false), m_offset(40) {
 	position(m_position);
+
+	m_largeur = m_position.x
+		+ (m_optionActive.largeur() > m_optionInactive.largeur() ?
+			m_optionActive.largeur(): m_optionInactive.largeur());
+	m_hauteur = m_texte.hauteur();
+
+	redimensionner(m_largeur, m_hauteur);
 }
 
-Option::~Option() {
-	SDL_FreeSurface(m_blocEffaceur);
-}
+Option::~Option()
+{}
 
 void Option::position(const SDL_Rect& position) {
 	m_position.x = position.x;
@@ -25,11 +30,6 @@ void Option::position(const SDL_Rect& position) {
 	m_position.x+= m_offset + m_texte.largeur();
 	m_optionActive.position(m_position);
 	m_optionInactive.position(m_position);
-
-	m_largeur = m_position.x
-		+ (m_optionActive.largeur() > m_optionInactive.largeur() ?
-			m_optionActive.largeur(): m_optionInactive.largeur());
-	m_hauteur = m_texte.hauteur();
 }
 
 void Option::position(const SDL_Rect& positionOption,
@@ -45,6 +45,8 @@ void Option::position(const SDL_Rect& positionOption,
 			+ (m_optionActive.largeur() > m_optionInactive.largeur() ?
 				m_optionActive.largeur(): m_optionInactive.largeur());
 	m_hauteur = positionEtat.y - m_position.y + m_texte.hauteur();
+
+	redimensionner(m_largeur, m_hauteur);
 }
 
 void Option::activer()
@@ -57,14 +59,9 @@ void Option::echanger()
 {	m_active = !m_active;	}
 
 void Option::afficher(SDL_Surface* ecran) {
-	if (NULL==m_blocEffaceur) {
-		m_blocEffaceur = SDL_CreateRGBSurface(SDL_HWSURFACE,
-				m_largeur, m_hauteur, 32, 0, 0, 0, 0);
-		SDL_FillRect(m_blocEffaceur, NULL,
-				SDL_MapRGB(ecran->format, 0, 0, 0));
-	}
+	effacer();
+	Ecran::afficher(ecran);
 
-	SDL_BlitSurface(m_blocEffaceur, NULL, ecran, &m_position);
 	m_texte.afficher(ecran);
 	if (m_active) {
 		m_optionActive.afficher(ecran);
